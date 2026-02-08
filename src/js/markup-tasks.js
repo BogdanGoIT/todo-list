45f066f9c8d339494a17fa8812061b6ed8226e07
Pako_Tako <pavel.korschov@gmail.com>
2026-02-08 12:00:04 +0100

    Tasksa update and delete

--- src/js/markup-tasks.js
+++ src/js/markup-tasks.js
@@ -1,6 +1,6 @@
 export function markupTasks(arr){
-    return arr.map(({title, description}) => `<li class="task-list-item">
-    <button class="task-list-item-btn">Delete</button>
+    return arr.map(({title, description, id}) => `<li class="task-list-item">
+    <button class="task-list-item-btn" data-id="${id}">Delete</button>
     <h3>${title}</h3>
     <p>${description}</p>
 </li>`).join("");
